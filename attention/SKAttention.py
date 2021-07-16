b74a16d91c2248a48c600d92113af0269d8eb8f6
xmu-xiaoma666 <775629340@qq.com>
2021-07-16 12:08:22 +0800

    sknet

--- attention/SKAttention.py
+++ attention/SKAttention.py
@@ -24,22 +24,9 @@ class SKAttention(nn.Module):
         self.fcs=nn.ModuleList([])
         for i in range(len(kernels)):
             self.fcs.append(nn.Linear(self.d,channel))
-        self.softmax=nn.Softmax(dim=1)
+        self.softmax=nn.Softmax(dim=0)
 
 
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
 
     def forward(self, x):
         bs, c, _, _ = x.size()
