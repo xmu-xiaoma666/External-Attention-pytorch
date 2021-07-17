b6a8fe2e628f0ed1f22c4c6c5f8157f23686ff5b
xmu-xiaoma666 <775629340@qq.com>
2021-07-17 13:06:43 +0800

    PSA

--- attention/PolarizedSelfAttention.py
+++ attention/PolarizedSelfAttention.py
@@ -11,7 +11,8 @@ class ParallelPolarizedSelfAttention(nn.Module):
         super().__init__()
         self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
         self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
-        self.softmax=nn.Softmax(1)
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
         self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
         self.ln=nn.LayerNorm(channel)
         self.sigmoid=nn.Sigmoid()
@@ -27,7 +28,7 @@ class ParallelPolarizedSelfAttention(nn.Module):
         channel_wq=self.ch_wq(x) #bs,1,h,w
         channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
         channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
-        channel_wq=self.softmax(channel_wq)
+        channel_wq=self.softmax_channel(channel_wq)
         channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
         channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
         channel_out=channel_weight*x
@@ -38,6 +39,7 @@ class ParallelPolarizedSelfAttention(nn.Module):
         spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
         spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
         spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
+        spatial_wq=self.softmax_spatial(spatial_wq)
         spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
         spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
         spatial_out=spatial_weight*x
@@ -55,7 +57,8 @@ class SequentialPolarizedSelfAttention(nn.Module):
         super().__init__()
         self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
         self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
-        self.softmax=nn.Softmax(1)
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
         self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
         self.ln=nn.LayerNorm(channel)
         self.sigmoid=nn.Sigmoid()
@@ -71,7 +74,7 @@ class SequentialPolarizedSelfAttention(nn.Module):
         channel_wq=self.ch_wq(x) #bs,1,h,w
         channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
         channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
-        channel_wq=self.softmax(channel_wq)
+        channel_wq=self.softmax_channel(channel_wq)
         channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
         channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
         channel_out=channel_weight*x
@@ -82,11 +85,11 @@ class SequentialPolarizedSelfAttention(nn.Module):
         spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
         spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
         spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
+        spatial_wq=self.softmax_spatial(spatial_wq)
         spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
         spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
         spatial_out=spatial_weight*channel_out
-        out=spatial_out+channel_out
-        return out
+        return spatial_out
 
 
 
