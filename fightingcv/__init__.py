d7f9ca04954dda938f4bba9645b89beecb2679b6
xmu-xiaoma666 <775629340@qq.com>
2021-08-21 14:17:40 +0800

    new version

---
+++ fightingcv/__init__.py
@@ -0,0 +1,6 @@
+
+def test():
+    print ("hello world")
+
+if __name__ == '__main__':
+    test()
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from attention.gfnet import GFNet
-import torch
-from torch import nn
-from torch.nn import functional as F
+# from attention.gfnet import GFNet
+# import torch
+# from torch import nn
+# from torch.nn import functional as F
 
-x = torch.randn(1, 3, 224, 224)
-gfnet = GFNet(embed_dim=384, img_size=224, patch_size=16, num_classes=1000)
-out = gfnet(x)
-print(out.shape)
+# x = torch.randn(1, 3, 224, 224)
+# gfnet = GFNet(embed_dim=384, img_size=224, patch_size=16, num_classes=1000)
+# out = gfnet(x)
+# print(out.shape)
 
 
 # from backbone_cnn.resnext import ResNeXt50,ResNeXt101,ResNeXt152
@@ -19,4 +19,5 @@ print(out.shape)
 #     # resnext152=ResNeXt152(1000)
 #     out=resnext50(input)
 #     print(out.shape)
+
 
