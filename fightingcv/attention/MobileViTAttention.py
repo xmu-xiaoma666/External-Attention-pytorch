17a8c3dee51358a30125ddf9907e4190d16dfb94
xmu-xiaoma666 <775629340@qq.com>
2021-10-09 09:46:24 +0800

    MobileViT

---
+++ fightingcv/attention/MobileViTAttention.py
@@ -0,0 +1,44 @@
+from torch import nn
+import torch
+from torch.nn.modules import transformer
+from torch.nn.modules.transformer import Transformer
+from einops import rearrange
+
+class MobileViTAttention(nn.Module):
+    def __init__(self,in_channel=3,dim=512,kernel_size=3,patch_size=7):
+        super().__init__()
+        self.ph,self.pw=patch_size,patch_size
+        self.conv1=nn.Conv2d(in_channel,in_channel,kernel_size=kernel_size,padding=kernel_size//2)
+        self.conv2=nn.Conv2d(in_channel,dim,kernel_size=1)
+
+        self.trans=Transformer()
+
+        self.conv3=nn.Conv2d(dim,in_channel,kernel_size=1)
+        self.conv4=nn.Conv2d(2*in_channel,in_channel,kernel_size=kernel_size,padding=kernel_size//2)
+
+    def forward(self,x):
+        y=x.clone() #bs,c,h,w
+
+        ## Local Representation
+        y=self.conv2(self.conv1(x)) #bs,dim,h,w
+
+        ## Global Representation
+        _,_,h,w=y.shape
+        y=rearrange(y,'bs dim (nh ph) (nw pw) -> bs (ph pw) (nh nw) dim',ph=self.ph,pw=self.pw) #bs,h,w,dim
+        y=self.transformer(y)
+        y=rearrange(y,'bs (ph pw) (nh nw) dim -> bs dim (nh ph) (nw pw)',ph=self.ph,pw=self.pw,nh=h//self.ph,nw=w//self.pw) #bs,dim,h,w
+
+        ## Fusion
+        y=self.conv3(y) #bs,dim,h,w
+        y=torch.cat([x,y],1) #bs,2*dim,h,w
+        y=self.conv4(y) #bs,c,h,w
+
+        return y
+
+
+if __name__ == '__main__':
+    m=MobileViTAttention()
+    input=torch.randn(1,3,49,49)
+    output=m(input)
+    print(output.shape)
+    
