d0f7522536daf50c432440156a2e458d507e55ed
wmkai <39148461+wmkai@users.noreply.github.com>
2022-12-21 17:06:54 +0800

    add mobilenetv3_large_100 func

--- model/backbone/MobileNetV3.py
+++ model/backbone/MobileNetV3.py
@@ -549,3 +549,8 @@ def mobilenetv3_large_075(pretrained=False, **kwargs):
     model = _gen_mobilenet_v3('mobilenetv3_large_075', 0.75, pretrained=pretrained, **kwargs)
     return 
 
+@register_model
+def mobilenetv3_large_100(pretrained=False, **kwargs):
+    """ MobileNet V3 """
+    model = _gen_mobilenet_v3('mobilenetv3_large_100', 1.0, pretrained=pretrained, **kwargs)
+    return model
