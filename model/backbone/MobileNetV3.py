0d31669163ce3323b91c72c60debcc75d10dd565
wmkai <39148461+wmkai@users.noreply.github.com>
2022-12-21 17:09:10 +0800

    add tf_mobilenetv3_large_075 func

--- model/backbone/MobileNetV3.py
+++ model/backbone/MobileNetV3.py
@@ -597,3 +597,11 @@ def mobilenetv3_rw(pretrained=False, **kwargs):
         kwargs['bn_eps'] = BN_EPS_TF_DEFAULT
     model = _gen_mobilenet_v3_rw('mobilenetv3_rw', 1.0, pretrained=pretrained, **kwargs)
     return model
+
+@register_model
+def tf_mobilenetv3_large_075(pretrained=False, **kwargs):
+    """ MobileNet V3 """
+    kwargs['bn_eps'] = BN_EPS_TF_DEFAULT
+    kwargs['pad_type'] = 'same'
+    model = _gen_mobilenet_v3('tf_mobilenetv3_large_075', 0.75, pretrained=pretrained, **kwargs)
+    return model
