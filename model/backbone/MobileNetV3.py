e889413bb05790a236588ce88708081b11ce511f
wmkai <39148461+wmkai@users.noreply.github.com>
2022-12-21 16:49:05 +0800

    Create MobileNetV3.py

---
+++ model/backbone/MobileNetV3.py
@@ -0,0 +1,21 @@
+""" MobileNet V3
+A PyTorch impl of MobileNet-V3, compatible with TF weights from official impl.
+Paper: Searching for MobileNetV3 - https://arxiv.org/abs/1905.02244
+Hacked together by / Copyright 2019, Ross Wightman
+"""
+from functools import partial
+from typing import List
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD, IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD
+from timm.layers import SelectAdaptivePool2d, Linear, create_conv2d, get_norm_act_layer
+from ._builder import build_model_with_cfg, pretrained_cfg_for_features
+from ._efficientnet_blocks import SqueezeExcite
+from ._efficientnet_builder import EfficientNetBuilder, decode_arch_def, efficientnet_init_weights, \
+    round_channels, resolve_bn_args, resolve_act_layer, BN_EPS_TF_DEFAULT
+from ._features import FeatureInfo, FeatureHooks
+from ._manipulate import checkpoint_seq
+from ._registry import register_model
