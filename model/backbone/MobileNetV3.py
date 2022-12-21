0806ac00c061a0450332aa07c0112619c47b909a
wmkai <39148461+wmkai@users.noreply.github.com>
2022-12-21 17:09:50 +0800

    add tf_mobilenetv3_large_minimal_100 func

--- model/backbone/MobileNetV3.py
+++ model/backbone/MobileNetV3.py
@@ -613,3 +613,11 @@ def tf_mobilenetv3_large_100(pretrained=False, **kwargs):
     kwargs['pad_type'] = 'same'
     model = _gen_mobilenet_v3('tf_mobilenetv3_large_100', 1.0, pretrained=pretrained, **kwargs)
     return model
+
+@register_model
+def tf_mobilenetv3_large_minimal_100(pretrained=False, **kwargs):
+    """ MobileNet V3 """
+    kwargs['bn_eps'] = BN_EPS_TF_DEFAULT
+    kwargs['pad_type'] = 'same'
+    model = _gen_mobilenet_v3('tf_mobilenetv3_large_minimal_100', 1.0, pretrained=pretrained, **kwargs)
+    return model
