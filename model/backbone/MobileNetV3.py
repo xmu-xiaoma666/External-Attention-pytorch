33254deb0609c54f6a490496d8357f6b3d747ee8
wmkai <39148461+wmkai@users.noreply.github.com>
2022-12-21 16:50:07 +0800

    add _cfg

--- model/backbone/MobileNetV3.py
+++ model/backbone/MobileNetV3.py
@@ -19,3 +19,15 @@ from ._efficientnet_builder import EfficientNetBuilder, decode_arch_def, efficie
 from ._features import FeatureInfo, FeatureHooks
 from ._manipulate import checkpoint_seq
 from ._registry import register_model
+
+__all__ = ['MobileNetV3', 'MobileNetV3Features']
+
+
+def _cfg(url='', **kwargs):
+    return {
+        'url': url, 'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': (7, 7),
+        'crop_pct': 0.875, 'interpolation': 'bilinear',
+        'mean': IMAGENET_DEFAULT_MEAN, 'std': IMAGENET_DEFAULT_STD,
+        'first_conv': 'conv_stem', 'classifier': 'classifier',
+        **kwargs
+    }
